fc9775f2a82af5218b590244d2f0c7e5441cfd59
Gabriel Oliveira <gabrieloliveira@MacBook-Air-de-Gabriel.local>
2026-09-04 10:52:22 -0300

    feat: flags

--- app/load-engine/internal/metrics/metrics.go
+++ app/load-engine/internal/metrics/metrics.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 	"time"
 
@@ -19,6 +20,7 @@ type Metrics struct {
     Failures      int
     StatusCodeStats map[int]int
     TotalLatency time.Duration
+	Latencies []time.Duration
 }
 
 func (m *Metrics) Start() {
@@ -64,6 +66,7 @@ func (m *Metrics) Add(result httpclient.Result) {
 
 	m.StatusCodeStats[result.StatusCode]++
 	m.TotalLatency += result.Latency
+	m.Latencies = append(m.Latencies, result.Latency)
 
 	if result.StatusCode >= 200 && result.StatusCode < 300 {
 		m.Successes++
@@ -73,11 +76,11 @@ func (m *Metrics) Add(result httpclient.Result) {
 }
 
 func (m *Metrics) AverageLatency() time.Duration {
-	if m.TotalRequests == 0 {
+	if len(m.Latencies) == 0 {
 		return 0
 	}
 
-	return m.TotalLatency / time.Duration(m.TotalRequests)
+	return m.TotalLatency / time.Duration(len(m.Latencies))
 }
 
 func (m *Metrics) SuccessRate() float64 {
@@ -88,12 +91,39 @@ func (m *Metrics) SuccessRate() float64 {
 	return float64(m.Successes) / float64(m.TotalRequests)
 }
 
+func (m *Metrics) Percentile(p float64) time.Duration {
+	if len(m.Latencies) == 0 {
+		return 0
+	}
+
+	n := len(m.Latencies)
+	sorted := make([]time.Duration, n)
+	copy(sorted, m.Latencies)
+
+	sort.Slice(sorted, func(i, j int) bool {
+		return sorted[i] < sorted[j]
+	})
+
+	k := int(float64(n) * p / 100.0)
+
+	if k < 0 {
+		k = 0
+	}
+	if k >= n {
+		k = n - 1
+	}
+	return sorted[k]
+}
+
 func (m *Metrics) Report() string {
 	return fmt.Sprintf(
-		"Total Requests: %d\nSuccesses: %d\nFailures: %d\nAverage Latency: %s\nSuccess Rate: %.2f%%\nStatus Code Stats: %v\nRPS: %.2f\nDuration: %s",
+		"Total Requests: %d\nSuccesses: %d\nFailures: %d\n50th Percentile Latency: %s\n95th Percentile Latency: %s\n99th Percentile Latency: %s\nAverage Latency: %s\nSuccess Rate: %.2f%%\nStatus Code Stats: %v\nRPS: %.2f\nDuration: %s",
 		m.TotalRequests,
 		m.Successes,
 		m.Failures,
+		m.Percentile(50),
+		m.Percentile(95),
+		m.Percentile(99),
 		m.AverageLatency(),
 		m.SuccessRate()*100,
 		m.StatusCodeStats,
